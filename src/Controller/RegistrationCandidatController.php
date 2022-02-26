d81465fcd80a7e7458e1a5efde9cb24855d7206a
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-02-26 20:12:28 +0100

    Create the registers forms for candidat, consultant et recruteur

---
+++ src/Controller/RegistrationCandidatController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationCandidatFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class RegistrationCandidatController extends AbstractController
+{
+    #[Route('/registerCandidat', name: 'app_registerCandidat')]
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationCandidatFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_accueil');
+        }
+
+        return $this->render('registration/registerCandidat.html.twig', [
+            'registrationCandidatForm' => $form->createView(),
+        ]);
+    }
+}
