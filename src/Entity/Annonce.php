95c383610dc61ab2b210797173e5910c77a69955
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-03-04 19:26:15 +0100

    Fix displaying of applied announce for candidat

--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -133,12 +133,15 @@ class Annonce
 
     public function checkIfPostulant(Annonce $annonce,Candidat $candidat)
     {
+        $isNotAlreadyCandidat = true;
         $postulants = $annonce->getPostulants();
         if(count($postulants) and count($postulants)>0){
             foreach ($postulants as $postulant){
-                    return $postulant->getCandidat() == $candidat;
+              if($postulant->getCandidat()[0] === $candidat){
+                $isNotAlreadyCandidat= false; break;
+               }
             }
         }
-        return true;
+        return $isNotAlreadyCandidat;
     }
 }
