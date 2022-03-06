9926f571724b910c9213e40bf5585e857d78a2a4
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-03-06 14:09:03 +0100

    Fix of path wirking and add public/uploads in gitIgnore to avoid to remove uploaded files

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -18,7 +18,6 @@ class MailerController extends AbstractController
     public function sendEmail(MailerInterface $mailer,Annonce $annonce,Postulant $postulant): Response
     {
         $cvPath = $this->getParameter('kernel.project_dir').'/public/uploads/article_image/'.$postulant->getCandidat()[0]->getCvname();
-        dump($cvPath);
         $email = (new Email())
             ->from('brad@sandbox97fca9b4222d469192b1eb1f0ca0556f.mailgun.org')
             ->to('nat.aesh@orange.fr')
@@ -29,7 +28,7 @@ class MailerController extends AbstractController
             //->priority(Email::PRIORITY_HIGH)
             ->subject($postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom(). ' vous propose ses services')
             //->text('Vous trouverez son cv ici : http://hoteljobs.herokuapp.com/uploads/article_image/'.$cvPath)
-            ->html('<p>Nous avons trouvé un postulant pour votre offre : '.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom().'Vous trouverez son cv ici : </p><a href="https://hoteljobs.herokuapp.com/uploads/article_image/' . $postulant->getCandidat()[0]->getCvname().'">'.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom().'</a>')
+            ->html('<p>Nous avons trouvé un postulant pour votre offre : '.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom().'. Vous trouverez son cv ici : </p><a href="https://hoteljobs.herokuapp.com/uploads/article_image/' . $postulant->getCandidat()[0]->getCvname().'">'.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom().'</a>')
             ->attachFromPath($cvPath)
             ;
 
