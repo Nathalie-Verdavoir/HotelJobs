806e475e73a7e6d2265045ae5843928d8051aee8
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-03-06 09:50:43 +0100

    check with dump

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -17,6 +17,7 @@ class MailerController extends AbstractController
     public function sendEmail(MailerInterface $mailer,Annonce $annonce,Postulant $postulant): Response
     {
         $cvPath = "/uploads/article_image/".$postulant->getCandidat()[0]->getCvname();
+        dump($cvPath);
         $email = (new Email())
             ->from('brad@sandbox97fca9b4222d469192b1eb1f0ca0556f.mailgun.org')
             ->to('nat.aesh@orange.fr')
