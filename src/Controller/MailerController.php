7db75e5e5cca2da74711180e696083ab5e84e887
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-03-06 09:29:40 +0100

    test with short path

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -16,6 +16,7 @@ class MailerController extends AbstractController
     #[Route('/email/{annonce}/{postulant}', name: 'app_mail', methods: ['GET','POST'])]
     public function sendEmail(MailerInterface $mailer,Annonce $annonce,Postulant $postulant): Response
     {
+        $cvPath = "/uploads/article_image/".$postulant->getCandidat()[0]->getCvname();
         $email = (new Email())
             ->from('brad@sandbox97fca9b4222d469192b1eb1f0ca0556f.mailgun.org')
             ->to('nat.aesh@orange.fr')
@@ -27,7 +28,7 @@ class MailerController extends AbstractController
             ->subject('Time for Symfony Mailer!')
             ->text('Nous avons trouvé un postulant pour votre offre : '.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom())
             //->html('<p>Nous avons trouvé un postulant pour votre offre : </p><a href="https://hoteljobs.herokuapp.com/uploads/article_image/'.$postulant->getCandidat()[0]->getCvname().'">'.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom().'</a>')
-           // ->attachFromPath("https://hoteljobs.herokuapp.com/uploads/article_image/".$postulant->getCandidat()[0]->getCvname())
+            ->attachFromPath($cvPath)
             ;
 
         $mailer->send($email);
