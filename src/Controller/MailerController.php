6f1f81033d77a9f608c0f4194beb4755e5cf3173
Nathalie Verdavoir <nathalie.verdavoir@laposte.net>
2022-03-06 12:48:45 +0100

    Fix path with kernel

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -13,10 +13,11 @@ use Symfony\Component\Mime\Email;
 
 class MailerController extends AbstractController
 {
+    #[Security("is_granted('ROLE_CONSULTANT')", statusCode: 404)]
     #[Route('/email/{annonce}/{postulant}', name: 'app_mail', methods: ['GET','POST'])]
     public function sendEmail(MailerInterface $mailer,Annonce $annonce,Postulant $postulant): Response
     {
-        $cvPath = $postulant->getCandidat()[0]->getCvname();
+        $cvPath = $this->getParameter('kernel.project_dir').'/public/uploads/article_image/'.$postulant->getCandidat()[0]->getCvname();
         dump($cvPath);
         $email = (new Email())
             ->from('brad@sandbox97fca9b4222d469192b1eb1f0ca0556f.mailgun.org')
@@ -29,7 +30,7 @@ class MailerController extends AbstractController
             ->subject($postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom(). ' vous propose ses services')
             //->text('Vous trouverez son cv ici : http://hoteljobs.herokuapp.com/uploads/article_image/'.$cvPath)
             ->html('<p>Nous avons trouvé un postulant pour votre offre : '.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom().'Vous trouverez son cv ici : </p><a href="https://hoteljobs.herokuapp.com/uploads/article_image/' . $postulant->getCandidat()[0]->getCvname().'">'.$postulant->getCandidat()[0]->getUserid()->getPrenom().' '.$postulant->getCandidat()[0]->getUserid()->getNom().'</a>')
-            //->attachFromPath($cvPath)
+            ->attachFromPath($cvPath)
             ;
 
         $mailer->send($email);
